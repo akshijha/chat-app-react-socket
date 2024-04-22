7a101d2a9a7e112ba7b8d3481e3bf47316a453a1
MEENAKSHI JHA <meenakshi03jha@gmail.com>
2024-04-22 19:24:36 +0530

    chat-app
    chatting

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import {ChatEngine}  from 'react-chat-engine';
+
+import './App.css';
+
+const App=() =>{
+return(
+    <ChatEngine
+    height="100vh"
+    projectID="bd3d60f1-120f-4a8c-96ce-57e8c9b69672"
+    userName="Akshi"
+    userSecret="2323"
+
+    />
+)
+}
+export default App;
